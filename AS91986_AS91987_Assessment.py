dfa431ceb25a0e7125d734e15c8690fabf2d5733
Muzzamil-school <st22025@ormiston.school.nz>
2023-05-17 11:41:46 +1200

    lollies test

--- AS91986_AS91987_Assessment.py
+++ AS91986_AS91987_Assessment.py
@@ -1,1 +1,4 @@
-first commit
+lollies=int(input("how many lollies do you have "))
+students=int(input("how do you students have "))
+lollies_per_student=lollies//students
+print("the amount of lollies each student will have is {}".format(lollies_per_student))
