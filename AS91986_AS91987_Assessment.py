c2cc5b5dbe84d90638a7de73da886bc693454a3b
Muzzamil-school <st22025@ormiston.school.nz>
2023-05-22 15:35:04 +1200

    main page

--- AS91986_AS91987_Assessment.py
+++ AS91986_AS91987_Assessment.py
@@ -4,11 +4,11 @@ from tkinter import Tk
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
-from random import choice
-#main page
 
+#main page
 root=tk.Tk()
 root.title("Julie's Party Hire")
 root.geometry("800x600")
 root.configure(bg='cadet blue')
 
+mainloop()
