960bad17594466c731160e2cf19e04c320ebb9fe
Muzzamil-school <st22025@ormiston.school.nz>
2023-05-23 09:55:37 +1200

    logo and title

--- AS91986_AS91987_Assessment.py
+++ AS91986_AS91987_Assessment.py
@@ -19,14 +19,6 @@ logo_button=tk.Button(root, image=logo_image, height=70, width=120).grid(row=0, 
 title_label=tk.Label(root, text='Julies Party Hire', font=('Helvetica', 45), fg='black', bg='cadet blue')
 title_label.grid(row=0, column=1)
 
-#Labels
-customer_name_label=tk.Label(root, text='Customer Name', font=('Helvetica', 20), fg='black', bg='cadet blue').grid(row=1, column=0)
-receipt_number_label=tk.Label(root, text='Receipt Number', font=('Helvetica', 20), fg='black', bg='cadet blue').grid(row=2, column=0)
-item_hired_label=tk.Label(root, text='Item Hired', font=('Helvetica', 20), fg='black', bg='cadet blue').grid(row=3, column=0)
-num_item_hired_label=tk.Label(root, text='No. Items Hired', font=('Helvetica', 20), fg='black', bg='cadet blue').grid(row=4, column=0)
-delete_row_label=tk.Label(root, text='Delete Row', font=('Helvetica', 20), fg='black', bg='cadet blue').grid(row=5, column=0)
-
-
 
 
 
