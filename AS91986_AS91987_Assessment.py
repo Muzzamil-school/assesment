ddac3a33c544b58b7411b35a51981780d2ad0cf1
Muzzamil-school <st22025@ormiston.school.nz>
2023-05-22 16:55:25 +1200

    logo and title 1

--- AS91986_AS91987_Assessment.py
+++ AS91986_AS91987_Assessment.py
@@ -11,4 +11,18 @@ root.title("Julie's Party Hire")
 root.geometry("800x600")
 root.configure(bg='cadet blue')
 
+#Add logo
+logo_image=tk.PhotoImage(file='Julies_Party_Hire.png')
+logo_button=tk.Button(root, image=logo_image, height=70, width=120)
+logo_button.grid(row=0, column=0)
+
+#Add title
+title_label=ttk.Label(root, text='Julies Party Hire', font=('Helvetica', 45), foreground='black')
+title_label.grid(row=0, column=1)
+
+
+
+
+
+
 mainloop()
