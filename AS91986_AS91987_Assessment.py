a782f5dc8d24bdf8523a8663ffd52291dade2e41
Muzzamil-school <st22025@ormiston.school.nz>
2023-05-22 15:32:21 +1200

    main page

--- AS91986_AS91987_Assessment.py
+++ AS91986_AS91987_Assessment.py
@@ -1,4 +1,14 @@
-lollies=int(input("how many lollies do you have "))
-students=int(input("how do you students have "))
-lollies_per_student=lollies//students
-print("the amount of lollies each student will have is {}".format(lollies_per_student))
+#Muzzamil
+#This program is so we know what items are hired out
+from tkinter import Tk
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+from random import choice
+#main page
+
+root=tk.Tk()
+root.title("Julie's Party Hire")
+root.geometry("800x600")
+root.configure(bg='cadet blue')
+
