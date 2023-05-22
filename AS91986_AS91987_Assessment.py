7a6e72875c7ac73a0b0938e9ba36c93f805269e2
Muzzamil-school <st22025@ormiston.school.nz>
2023-05-23 09:47:20 +1200

    labels

--- AS91986_AS91987_Assessment.py
+++ AS91986_AS91987_Assessment.py
@@ -5,21 +5,26 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
-#main page
+#Main page
 root=tk.Tk()
 root.title("Julie's Party Hire")
 root.geometry("800x600")
 root.configure(bg='cadet blue')
 
-#Add logo
+#Logo
 logo_image=tk.PhotoImage(file='Julies_Party_Hire.png')
-logo_button=tk.Button(root, image=logo_image, height=70, width=120)
-logo_button.grid(row=0, column=0)
+logo_button=tk.Button(root, image=logo_image, height=70, width=120).grid(row=0, column=0)
 
-#Add title
-title_label=ttk.Label(root, text='Julies Party Hire', font=('Helvetica', 45), foreground='black')
+#Title
+title_label=tk.Label(root, text='Julies Party Hire', font=('Helvetica', 45), fg='black', bg='cadet blue')
 title_label.grid(row=0, column=1)
 
+#Labels
+customer_name_label=tk.Label(root, text='Customer Name', font=('Helvetica', 20), fg='black', bg='cadet blue').grid(row=1, column=0)
+receipt_number_label=tk.Label(root, text='Receipt Number', font=('Helvetica', 20), fg='black', bg='cadet blue').grid(row=2, column=0)
+item_hired_label=tk.Label(root, text='Item Hired', font=('Helvetica', 20), fg='black', bg='cadet blue').grid(row=3, column=0)
+num_item_hired_label=tk.Label(root, text='No. Items Hired', font=('Helvetica', 20), fg='black', bg='cadet blue').grid(row=4, column=0)
+delete_row_label=tk.Label(root, text='Delete Row', font=('Helvetica', 20), fg='black', bg='cadet blue').grid(row=5, column=0)
 
 
 
