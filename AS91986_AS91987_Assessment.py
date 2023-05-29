64bbb4b02ee5c9ab7435bc2d12392cb17a9f40ff
Muzzamil-school <st22025@ormiston.school.nz>
2023-05-29 14:15:58 +1200

    nax

--- AS91986_AS91987_Assessment.py
+++ AS91986_AS91987_Assessment.py
@@ -12,9 +12,14 @@ root.title("Julie's Party Hire")
 root.geometry("1000x400")
 root.configure(bg='cadet blue')
 
+#title julies party hire
+title_label=tk.Label(root, text='Julies Party Hire', font=('Helvetica', 45), fg='black', bg='cadet blue')
+title_label.grid(row=0, column=1, sticky='w')
 
-
-
+#add logo for julies party hire
+logo_image=tk.PhotoImage(file='Julies_Party_Hire.png')
+logo=tk.Label(root, image=logo_image, height=70, width=120)
+logo.grid(row=0, column=0, sticky='nw')
 
 
 mainloop()
